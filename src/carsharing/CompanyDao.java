df9518db08fae8e75fd1702dc8aec89143e90321
Denis Stoyanouski <vandenst@tut.by>
2023-01-20 21:35:57 +0300

    added imterface CompanyDao

---
+++ src/carsharing/CompanyDao.java
@@ -0,0 +1,10 @@
+package carsharing;
+
+import java.util.List;
+
+public interface CompanyDao {
+    public List<Company> getAllStudents();
+    public Company getStudent(int rollNo);
+    public void updateStudent(Company student);
+    public void deleteStudent(Company student);
+}
